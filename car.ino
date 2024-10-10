273c1cb00fabcad1a4dbdbc794248f0fc8b9a85e
Jesung Park <jesung.park@hotmail.com>
2024-10-10 19:46:07 +0100

    Incorporate LoRa
    Enable sending of sensor data via LoRa module. Runs setup code and can send strings via lora_send().
    
    Will refactor into its own file later...

--- car.ino
+++ car.ino
@@ -33,17 +33,109 @@ Sensor::CurrentSensor current(A11, A14, 1, 1);
 Sensor::TemperatureSensor motTemp(A8);
 Sensor::TemperatureSensor pcbTemp(A3);
 
+// for LoRa module
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#define RFM95_CS 22
+#define RFM95_RST 0
+#define RFM95_INT 2
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 434.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+int16_t packetnum = 0;
+
+
 bool sdStatus = false; //  Status of the SD card reader and radio
 
 //  Called for any error messages that occur
 void errorCallback(char* message) {
     Serial.println(message);//  Print the message to serial
-    //  Print the message to the text file if the SD card reader is ok
+//    //  Print the message to the text file if the SD card reader is ok
     if (sdStatus && txtFile) {
         txtFile.println(message);
         txtFile.flush();
         Serial.println("logged to text file");
     }
+}
+
+void lora_setup() {
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.println("Arduino LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+}
+
+void lora_send(String msg) {
+  int array_length = msg.length() + 1;
+  char radiopacket[array_length];
+  msg.toCharArray(radiopacket, array_length);
+  
+  Serial.println("Sending to rf95_server");
+  // Send a message to rf95_server
+  
+  Serial.print("Sending "); Serial.println(radiopacket);
+  radiopacket[array_length - 1] = 0;
+  
+  Serial.println("Sending..."); delay(10);
+  rf95.send((uint8_t *)radiopacket, array_length);
+
+  Serial.println("Waiting for packet to complete..."); delay(10);
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  Serial.println("Waiting for reply..."); delay(10);
+  if (rf95.waitAvailableTimeout(1000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is there a listener around?");
+  }
 }
 
 void setup() {
@@ -90,6 +182,9 @@ void setup() {
     sendLEDCommand(1,8);
     sendLEDCommand(2,8);
     sendLEDCommand(3,8);
+
+    // LoRa setup
+    lora_setup();
 }
 
 void loop() {
@@ -102,17 +197,23 @@ void reportCallback(double* results) {
     //  Called by the sensor manager to pass readings back to the main program
 
     //  Print all the readings to serial and the SD card text file
+    String msg = "";
+    
     for (int i = 0; i < sensorCount; i++) {
         if (i > 0) {
             Serial.print(",");
             txtFile.print(",");
+            msg += ",";
         }
         Serial.print(results[i]);
         txtFile.print(results[i]);
+        msg += String(results[i]);
     }
     Serial.println();
     txtFile.println();
     txtFile.flush();
+    
+    lora_send(msg);
 }
 
 void sendLEDCommand(int ledNumber, int state) {
@@ -121,6 +222,3 @@ void sendLEDCommand(int ledNumber, int state) {
   Wire.write(state);         // Send state
   Wire.endTransmission();
 }
-
-
-
